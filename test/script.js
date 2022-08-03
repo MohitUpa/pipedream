35cdbf7e18cecab060c7a4325f378046cb9c4cb2
Mohit Upadhyay <55775426+MohitUpa@users.noreply.github.com>
2022-08-03 06:08:39 -0700

    main file

---
+++ test/script.js
@@ -0,0 +1,49 @@
+
+
+let chart = new Vizzu('vizzuCanvas');
+
+
+
+chart.animate({
+    data: data,
+    config: {
+        channels: {
+            color: { set: ['Joy factors'] },
+            label: { set: ['Country_code'] },
+            /* With a dimension on the size channel Vizzu will stack
+            the elements by the categories on the other channels
+            on charts without coordinates. Here the Country code dimension is
+            used to stack the bubbles by the dimension on the color channel. */
+            size: { set: ['Country_code', 'Value 2 (+)'] }
+        },
+        title: 'Stacked Bubble Chart',
+        geometry: 'circle'
+    }
+});
+
+
+chart.animate({
+    config: {
+        channels: {
+            x: { set: ['Country_code', 'Value 2 (+)'] },
+            y: { set: ['Joy factors'] },
+            label: { set: null },
+            /* The stacking is eliminated when we remove
+            the extra dimension from the size channel. */
+            size: { set: null } 
+        },
+        title: 'Bar Chart',
+        geometry: 'rectangle',
+        orientation: 'vertical'
+    }
+});
+
+
+chart.animate({
+    config: {
+        channels: {
+            x: { detach: ['Country_code'] },
+            label: { set: ['Value 2 (+)'] }
+        }
+    }
+});
